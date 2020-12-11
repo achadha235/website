00b9eb3035159ccfaf7d0f4d2450a7acafa52827
Abhi Chadha <abhishekchadha235@gmail.com>
2020-12-12 01:01:45 +0530

    remove manifest

--- src/manifest.ts
+++
@@ -1,1 +0,0 @@
-export * as Foo from 'src/pages/blog/12-20/my-resolutions.mdx';
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -5,9 +5,11 @@ import Layout from 'src/components/Layout';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import * as categories from 'src/categories';
-import * as posts from 'src/manifest';
+import glob from 'glob';
+import path from 'path';
+import fs from 'fs';
 
-function BlogHomepage() {
+function BlogHomepage({ blogPosts }) {
   return (
     <Layout className='h-full flex justify-center'>
       <Head>
@@ -44,13 +46,13 @@ function BlogHomepage() {
           </div>
         </div>
 
-        <div className='w-full mt-5'>
-          {Object.values(posts).map((postModule) => (
-            <Link key={postModule.meta.title} href={postModule.meta.path}>
-              <div className='w-full border-1 border-solid border-grey-700 p-5 cursor-pointer hover:border-blue-800'>
-                <Typography variant='h4'>{postModule.meta.title}</Typography>
+        <div className='w-full my-5 mb-20'>
+          {blogPosts.map((postModule) => (
+            <Link key={postModule.title} href={postModule.path}>
+              <div className='w-full border-1 border-solid border-grey-700 p-5 cursor-pointer hover:border-blue-800 my-5'>
+                <Typography variant='h4'>{postModule.title}</Typography>
                 <Typography variant='body2' className='leading-loose mt-4'>
-                  {postModule.meta.banner}
+                  {postModule.banner}
                 </Typography>
               </div>
             </Link>
@@ -60,5 +62,33 @@ function BlogHomepage() {
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const blogPaths = path.join(process.cwd(), 'src/pages/blog/**/*.mdx');
+  return {
+    props: {
+      blogPosts: glob.sync(blogPaths).map((filePath) => ({
+        ...getMetaObject(filePath),
+        path: getSlug(filePath),
+      })),
+    },
+  };
+}
+
+const getSlug = (blogPath) =>
+  blogPath
+    .toString()
+    .replace(path.join(process.cwd(), 'src/pages').toString(), '')
+    .replace('.mdx', '');
+
+const getMetaObject = (filePath) =>
+  new Function(
+    `${
+      fs
+        .readFileSync(filePath)
+        .toString()
+        .match(/(const meta = )(\{[\s\S]+?\})/)[0]
+    }; return meta;`
+  )();
 
 export default BlogHomepage;
