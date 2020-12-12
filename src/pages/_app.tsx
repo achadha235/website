9aead69bf2b54881f9a71e0d4a16415e1f6b1cec
Abhi Chadha <abhishekchadha235@gmail.com>
2020-12-12 14:42:40 +0530

    set up segment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,17 @@ function App({ Component, pageProps, router }) {
         <link rel='icon' href='images/favicon.svg'></link>
 
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.13.1";
+              analytics.load("tUhC8yMzZjhLOzhclpJDv07Iw7ABQld1");
+              analytics.page();
+              }}();
+            `,
+          }}
+        />
+
+        {/* <script
           async
           src='https://www.googletagmanager.com/gtag/js?id=G-L590B8S404'
         />
@@ -38,7 +49,7 @@ function App({ Component, pageProps, router }) {
               gtag('config', 'G-L590B8S404');
                   `,
           }}
-        />
+        /> */}
       </Head>
       <ThemeProvider theme={theme}>
         <AppContext.Provider value={appContext}>
